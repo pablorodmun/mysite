b7316ea44dab5c834d7a9162e2a471b898b81b97
PabloR <pablorodriguezmun@gmail.com>
2023-11-06 15:24:20 -0300

    Mi app con django, primer commit

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Post(models.Model):  ## se crea el modelo post. models.Model significa que Post es un modelo de Django, así Django sabe que debe guardarlo en la base de datos.
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self): ## metodo que publica
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
